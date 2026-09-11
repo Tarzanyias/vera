72c492da2b5867051135970f145353f5c9583ef4
Tarzanyias <tarzanya793@gmail.com>
2026-09-11 18:37:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ import { createServer } from "node:http";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.send("WISP Relay active!");
 });
 
 const server = createServer(app);
 
+// Direct all WebSocket upgrade requests into WISP
 server.on("upgrade", (req, socket, head) => {
   wisp.routeRequest(req, socket, head);
 });
 
 server.listen(PORT, () => {
-  console.log(`WISP server active on port ${PORT}`);
+  console.log(`WISP server listening on port ${PORT}`);
 });
